cd24380a3ea0f416842a96b1bb1718da5c2717be
Yasaswini720 <97751289+Yasaswini720@users.noreply.github.com>
2023-04-07 13:50:15 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2>Contact Manager(Demo3)</h2>
+        <h2>Contact Manager(Demo4)</h2>
       </div>
     </div>
   );
